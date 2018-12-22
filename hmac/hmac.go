0c420ecb677232da403041e9f54373d894df1656
Bjärn Bnegtsson <drgevalia@gmail.com>
2018-12-22 13:02:40 +0100

    Sign now returns a hex encoded byte array

--- hmac/hmac.go
+++ hmac/hmac.go
@@ -2,13 +2,14 @@ package hmac
 
 import (
 	"crypto/hmac"
+	"encoding/hex"
 	"hash"
 )
 
 func Sign(key, body []byte, provider func() hash.Hash) []byte {
 	hasher := hmac.New(provider, key)
 	hasher.Write(body)
-	return hasher.Sum(nil)
+	return []byte(hex.EncodeToString(hasher.Sum(nil)))
 }
 
 func Verify(key, body, signature []byte, provider func() hash.Hash) bool {
