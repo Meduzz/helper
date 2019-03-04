3b5427084d7ec23a09f8a7fe2d657f5362a524ba
Bjärn Bnegtsson <drgevalia@gmail.com>
2019-03-04 18:17:49 +0100

    Adds my most comonly used hashing funcs.

---
+++ hashing/hash.go
@@ -0,0 +1,47 @@
+package hashing
+
+import (
+	"crypto/rand"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"hash"
+)
+
+func Hash(data []byte, hasher hash.Hash) string {
+	hasher.Write(data)
+	bs := hasher.Sum(nil)
+
+	return hex.EncodeToString(bs)
+}
+
+func Token() string {
+	randomBytes, err := Random(1024)
+
+	if err != nil {
+		return Token()
+	}
+
+	return Hash(randomBytes, sha1.New())
+}
+
+func Secret() string {
+	randomBytes, err := Random(1024)
+
+	if err != nil {
+		return Secret()
+	}
+
+	return Hash(randomBytes, sha256.New())
+}
+
+func Random(size int) ([]byte, error) {
+	bs := make([]byte, size)
+	_, err := rand.Read(bs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bs, nil
+}
