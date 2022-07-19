5e5efd3a8578456e8b7e2522e887c0cfb6952e0b
Meduzz <drgevalia@gmail.com>
2022-07-19 11:25:28 +0200

    adding wendymodules starter bump deps

---
+++ starters/wendymodules.go
@@ -0,0 +1,40 @@
+package starters
+
+import (
+	"fmt"
+
+	"github.com/Meduzz/helper/nuts"
+	"github.com/Meduzz/wendy"
+	"github.com/spf13/cobra"
+)
+
+func WendyModules(modules ...*wendy.Module) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "start a few wendy module on top of a nats conn",
+	}
+
+	queue := cmd.Flags().StringP("queue", "q", "", "set queue group or leave empty")
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		conn, err := nuts.Connect()
+
+		if err != nil {
+			return err
+		}
+
+		if *queue != "" {
+			for _, m := range modules {
+				conn.QueueSubscribe(fmt.Sprintf("%s.#", m.Name()), *queue, wrapModule(m))
+			}
+		} else {
+			for _, m := range modules {
+				conn.Subscribe(fmt.Sprintf("%s.#", m.Name()), wrapModule(m))
+			}
+		}
+
+		return nil
+	}
+
+	return cmd
+}
